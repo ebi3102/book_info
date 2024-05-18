df586835a043a4636bcd904fff533262127a9edf
ebi3102 <ebi3102@gmail.com>
2024-05-18 17:51:22 +0330

    Add  Rabbit\Plugin applicatiom as property to the instance class

--- src/Book_Info.php
+++ src/Book_Info.php
@@ -7,9 +7,16 @@
  */
 namespace BookInfo;
 use BookInfo\Repositories\Create_Book_Info_Table;
+use Rabbit\Plugin;
 
 final class Book_Info
 {
+    private Plugin $application;
+
+    public function __construct(Plugin $application)
+    {
+        $this->application = $application;
+    }
     public function onActivation()
     {
         new Create_Book_Info_Table();
