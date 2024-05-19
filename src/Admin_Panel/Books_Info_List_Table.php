0826004e8b272fbad47e7612ec2b4cdf74924065
ebi3102 <ebi3102@gmail.com>
2024-05-19 16:32:26 +0330

    Modify Books_Info_List_Table class

--- src/Admin_Panel/Books_Info_List_Table.php
+++ src/Admin_Panel/Books_Info_List_Table.php
@@ -1,17 +1,23 @@
 <?php
 namespace BookInfo\Admin_Panel;
+use BookInfo\Controllers\Get_All_Books_Info;
 if (!class_exists('WP_List_Table')) {
     require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
 }
 
 class Books_Info_List_Table extends \WP_List_Table {
 
-    function __construct() {
+    public string $table_name;
+    public int $per_page;
+
+    function __construct(string $table_name, int $per_page=10) {
         parent::__construct([
             'singular' => 'Book Info',
             'plural'   => 'Books Info',
             'ajax'     => false 
         ]);
+        $this->table_name = $table_name;
+        $this->per_page = $per_page;
     }
 
     function get_columns() {
@@ -49,25 +55,19 @@ class Books_Info_List_Table extends \WP_List_Table {
         ];
     }
 
-    function prepare_items($per_page=10) {
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'books_info';
+    function prepare_items() {
+        $booksInfoObj = new Get_All_Books_Info($this->table_name);
+        $total_items = $booksInfoObj->total_items();
 
-        $query = "SELECT * FROM $table_name";
-        $total_items = $wpdb->query($query);
-
-        $per_page = 10;
         $current_page = $this->get_pagenum();
-        $total_pages = ceil($total_items / $per_page);
+        $total_pages = ceil($total_items / $this->per_page);
 
         $this->set_pagination_args([
             'total_items' => $total_items,
-            'per_page'    => $per_page,
+            'per_page'    => $this->per_page,
             'total_pages' => $total_pages
         ]);
 
-        $query .= " LIMIT " . (($current_page - 1) * $per_page) . ", $per_page";
-        $this->items = $wpdb->get_results($query, ARRAY_A);
-        // var_dump($this->items);
+        $this->items = $booksInfoObj->get_limited_items($current_page, $this->per_page);
     }
 }
